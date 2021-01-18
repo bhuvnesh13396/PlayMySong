ae9afad5ede032af3202b42d6b0b654e91e6c00b
bhuvi <bhuvnesh13396@gmail.com>
2021-01-18 11:07:07 +0530

    Added queries to like service

--- repo/mongo/likeRepo.go
+++ repo/mongo/likeRepo.go
@@ -2,9 +2,12 @@ package mongo
 
 import (
 	"fmt"
-
+	"log"
+	"context"
 	"github.com/bhuvnesh13396/PlayMySong/model"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	//"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type likeRepo struct {
@@ -19,13 +22,13 @@ func NewLikeRepo(db *mongo.Client) (*likeRepo, error) {
 
 func (repo *likeRepo) Add(a model.Like) (err error) {
 
-	// Add entry in the likes table
-	// collection := repo.db.Database("test").Collection("likes")
-	// _, err = collection.InsertOne(context.TODO(), a)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// 	return
-	// }
+	//Add entry in the likes table
+	collection := repo.db.Database("test").Collection("likes")
+	_, err = collection.InsertOne(context.TODO(), a)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// Increment the value of total likes for the given activity
 	// in the aggregation table
@@ -38,30 +41,29 @@ func (repo *likeRepo) Add(a model.Like) (err error) {
 }
 
 func (repo *likeRepo) Get(activityID string) (count int, err error) {
-	// collection := repo.db.Database("test").Collection("activities")
-	// filter := bson.D{{"activity_id", activityID}, {"count": 1}}
-	// var result bson.M
-	// err = collection.FindOne(context.TODO(), filter).Decode(&result)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("Result ", result)
-	// fmt.Printf("Found a single document: %+v\n", result.count)
-	// return result.count, nil
+	collection := repo.db.Database("test").Collection("activities")
+	filter := bson.D{{"activity_id", activityID}}
+	var result bson.M
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Result ", result)
+	fmt.Printf("Found a single document: %+v\n", result["count"])
 
-	return
+	return result["count"].(int), nil
 }
 
 func (repo *likeRepo) Delete(activityID string, userID string) (err error) {
 
-	// Delete the entry from the likes table
-	// collection := repo.db.Database("test").Collection("likes")
-	// filter := bson.D{{"activity_id": activityID, "user_id": userID}}
-	// _, err = collection.DeleteOne(context.TODO(), collection.FindOne(context.TODO(), filter))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// 	return
-	// }
+	//Delete the entry from the likes table
+	collection := repo.db.Database("test").Collection("likes")
+	filter := bson.D{{"activity_id", activityID},{"user_id", userID}}
+	_, err = collection.DeleteOne(context.TODO(), collection.FindOne(context.TODO(), filter))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// Decrement the value of total likes for given activity
 	// in the aggregation table
@@ -72,5 +74,19 @@ func (repo *likeRepo) Delete(activityID string, userID string) (err error) {
 }
 
 func (repo *likeRepo) UpdateCount(activityID string, count int) (err error) {
-	return nil
+	collection := repo.db.Database("test").Collection("activities")
+	filter := bson.D{{"activity_id", activityID}}
+	update := bson.D{
+		{"$set", bson.D{
+			{"count", count},
+		}},
+	}
+
+	_, err = collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Like count updated !")
+	return
 }
